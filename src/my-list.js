d807c303b99bf15e60e58d2952717e6516b51cf2
Gavin Farnsworth <gbfarns16@gmail.com>
2017-07-07 22:42:07 -0600

    Finished firebase sample

--- src/my-list.js
+++ src/my-list.js
@@ -3,5 +3,16 @@ Polymer({
     behaviors: [Polymer.NoteAppBehavior],
     signIn: function () {
         this.$.auth.signInWithPopup();
+    },
+
+    get notesPath() {
+        return '/notes/' + this.user.uid;
+    },
+
+    toEditableId: function (noteId) {
+        return this.notesPath + '/' + noteId;
+    },
+    get isEditable() {
+        return this.online;
     }
 });
